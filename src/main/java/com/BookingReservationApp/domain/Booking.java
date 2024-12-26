c74ee1280824ca6aba744ae7e60002beddde2e65
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-26 11:50:03 -0600

    Pojo class

---
+++ src/main/java/com/BookingReservationApp/domain/Booking.java
@@ -0,0 +1,66 @@
+package com.BookingReservationApp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Booking {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long eventId;
+    private String userName;
+    private String UserEmail;
+    private LocalDateTime bookingTime;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return UserEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        UserEmail = userEmail;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
