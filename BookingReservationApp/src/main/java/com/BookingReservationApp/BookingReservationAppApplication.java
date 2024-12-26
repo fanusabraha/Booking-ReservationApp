72045a53462dde66abc96e79344e7c66c6cc3df1
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-26 11:02:08 -0600

    booking-reservation App start

---
+++ BookingReservationApp/src/main/java/com/BookingReservationApp/BookingReservationAppApplication.java
@@ -0,0 +1,13 @@
+package com.BookingReservationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingReservationAppApplication.class, args);
+	}
+
+}
---
+++ BookingReservationApp/src/test/java/com/BookingReservationApp/BookingReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookingReservationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
