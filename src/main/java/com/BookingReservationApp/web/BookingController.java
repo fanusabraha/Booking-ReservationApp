67607e2616903f0b87aaca965907ca0ee5d2b756
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-27 11:24:41 -0600

    Update BookingController.java

--- src/main/java/com/BookingReservationApp/web/BookingController.java
+++ src/main/java/com/BookingReservationApp/web/BookingController.java
@@ -1,14 +1,24 @@
 package com.BookingReservationApp.web;
 
+import com.BookingReservationApp.domain.Booking;
 import com.BookingReservationApp.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/bookings")
 public class BookingController {
     @Autowired
     BookingService bookingService;
-    
+
+    @PostMapping
+    public ResponseEntity<Booking> createBooking(@RequestBody Booking booking){
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
